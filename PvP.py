56556d5a57d90bdea8ea199570bb774f48307611
Mickaël Bannholtzer <bannholtzer.mickael@gmail.com>
2023-01-31 14:23:20 +0100

    correctif pvp.py

--- PvP.py
+++ PvP.py
@@ -5,8 +5,21 @@ from numba import jit
 import numpy as np
 import time
 
+
 @jit(nopython=True)
-def  playout_IA100P_vs_AIRand(B):
+def  playout_IARand_vs_IARand(B):
+    while not gn.Terminated(B):        
+        # player 0
+        gn.PlayPvP(B) # play one time
+        
+        # player 1
+        gn.PlayPvP(B) # play one time
+        
+    return gn.GetScore(B)
+
+
+@jit(nopython=True)
+def  playout_IA100P_vs_IARand(B):
     while not gn.Terminated(B):        
         # player 0
         IA100P.play(B) # play one time
@@ -20,39 +33,67 @@ def  playout_IA100P_vs_AIRand(B):
 def  playout_IA1KP_vs_IA100P(B):
     while not gn.Terminated(B):        
         # player 0
-        IA10KP.play(B) # play one time
+        IA1KP.play(B) # play one time
         
         # player 1
-        IA1KP.play(B) # play one time
+        IA100P.play(B) # play one time
     return gn.GetScore(B)    
 
 @jit(nopython=True)
 def  playout_IA10KP_vs_IA1KP(B):
     while not gn.Terminated(B):        
         # player 0
-        IA1KP.play(B) # play one time
+        IA10KP.play(B) # play one time
         
         # player 1
-        IA100P.play(B) # play one time
+        IA1KP.play(B) # play one time
     return gn.GetScore(B)   
       
-@jit(nopython=True, parallel=True)
-def Parrallel_playout_IA100P_vs_AIRand(nb,StartingBoard:np.ndarray):
-    Scores = np.empty(nb)
-    for i in numba.prange(nb):
-        Scores[i] = playout_IA100P_vs_AIRand(StartingBoard.copy())
-    return Scores
+# @jit(nopython=True, parallel=True)
+# def Parrallel_playout_IA100P_vs_IARand(nb,StartingBoard:np.ndarray):
+#     Scores = np.empty(nb)
+#     for i in numba.prange(nb):
+#         Scores[i] = playout_IA100P_vs_IARand(StartingBoard.copy())
+#     return Scores
+
+# @jit(nopython=True, parallel=True)
+# def Parrallel_playout_IA1KP_vs_IA100P(nb,StartingBoard:np.ndarray):
+#     Scores = np.empty(nb)
+#     for i in numba.prange(nb):
+#         Scores[i] = playout_IA1KP_vs_IA100P(StartingBoard.copy())
+#     return Scores
 
 @jit(nopython=True, parallel=True)
-def Parrallel_playout_IA1KP_vs_IA100P(nb,StartingBoard:np.ndarray):
+def Parrallel_playout_IA10KP_vs_IA1KP(nb,StartingBoard:np.ndarray):
     Scores = np.empty(nb)
     for i in numba.prange(nb):
-        Scores[i] = playout_IA1KP_vs_IA100P(StartingBoard.copy())
+        Scores[i] = playout_IA10KP_vs_IA1KP(StartingBoard.copy())
     return Scores
 
 
 if __name__ == '__main__':
     
+    ##################################################################################
+    #           play IARand vs IARand
+    #
+    print("\nIA100P vs IARand")
+    nbSims = 10 * 100
+    winCountP0 = 0
+    winCountP1 = 0
+    StartingBoard = gn.CreateNewGame()
+    T0 = time.time()
+    for i in range (nbSims):
+        score = playout_IARand_vs_IARand(StartingBoard.copy())
+        if (score == 1):
+            winCountP0 += 1
+        elif (score == -1): 
+            winCountP1 += 1
+        elif (score == 0):
+            raise Exception("Error in the score")
+    dt = time.time() - T0
+    print ("IA100P:",winCountP0/nbSims*100,"\nIARand:",winCountP1/nbSims*100)
+    print ("time:",dt)
+    print ("nbsim/s:",nbSims/dt)
     
     ##################################################################################
     #           play IA100P vs IARand
@@ -64,7 +105,7 @@ if __name__ == '__main__':
     StartingBoard = gn.CreateNewGame()
     T0 = time.time()
     for i in range (nbSims):
-        score = playout_IA1KP_vs_IA100P(StartingBoard.copy())
+        score = playout_IA100P_vs_IARand(StartingBoard.copy())
         if (score == 1):
             winCountP0 += 1
         elif (score == -1): 
@@ -98,45 +139,45 @@ if __name__ == '__main__':
     ##################################################################################
     #           play IA1KP vs IA100P
     #
-    print("\nIA1KP vs IA100P")
-    nbSims = 10 * 100
-    winCountP0 = 0
-    winCountP1 = 0
-    StartingBoard = gn.CreateNewGame()
-    T0 = time.time()
-    for i in range (nbSims):
-        score = playout_IA1KP_vs_IA100P(StartingBoard.copy())
-        if (score == 1):
-            winCountP0 += 1
-        elif (score == -1): 
-            winCountP1 += 1
-        elif (score == 0):
-            raise Exception("Error in the score")
-    dt = time.time() - T0
-    print ("IA1kP:",winCountP0/nbSims*100,"\nIA100P:",winCountP1/nbSims*100)
-    print ("time:",dt)
-    print ("nbsim/s:",nbSims/dt)
+    # print("\nIA1KP vs IA100P")
+    # nbSims = 10 * 100
+    # winCountP0 = 0
+    # winCountP1 = 0
+    # StartingBoard = gn.CreateNewGame()
+    # T0 = time.time()
+    # for i in range (nbSims):
+    #     score = playout_IA1KP_vs_IA100P(StartingBoard.copy())
+    #     if (score == 1):
+    #         winCountP0 += 1
+    #     elif (score == -1): 
+    #         winCountP1 += 1
+    #     elif (score == 0):
+    #         raise Exception("Error in the score")
+    # dt = time.time() - T0
+    # print ("IA1kP:",winCountP0/nbSims*100,"\nIA100P:",winCountP1/nbSims*100)
+    # print ("time:",dt)
+    # print ("nbsim/s:",nbSims/dt)
     
     ##################################################################################
     #           play IA10KP vs IA1KP
     #
-    print("\nIA10KP vs IA1KP")
-    nbSims = 10 * 100
-    winCountP0 = 0
-    winCountP1 = 0
-    StartingBoard = gn.CreateNewGame()
-    T0 = time.time()
-    for i in range (nbSims):
-        score = playout_IA10KP_vs_IA1KP(StartingBoard.copy())
-        if (score == 1):
-            winCountP0 += 1
-        elif (score == -1): 
-            winCountP1 += 1
-        elif (score == 0):
-            raise Exception("Error in the score")
-    dt = time.time() - T0
-    print ("IA1kP:",winCountP0/nbSims*100,"\nIA100P:",winCountP1/nbSims*100)
-    print ("time:",dt)
-    print ("nbsim/s:",nbSims/dt)
+    # print("\nIA10KP vs IA1KP")
+    # nbSims = 10 * 100
+    # winCountP0 = 0
+    # winCountP1 = 0
+    # StartingBoard = gn.CreateNewGame()
+    # T0 = time.time()
+    # for i in range (nbSims):
+    #     score = playout_IA10KP_vs_IA1KP(StartingBoard.copy())
+    #     if (score == 1):
+    #         winCountP0 += 1
+    #     elif (score == -1): 
+    #         winCountP1 += 1
+    #     elif (score == 0):
+    #         raise Exception("Error in the score")
+    # dt = time.time() - T0
+    # print ("IA1kP:",winCountP0/nbSims*100,"\nIA100P:",winCountP1/nbSims*100)
+    # print ("time:",dt)
+    # print ("nbsim/s:",nbSims/dt)
 
     
